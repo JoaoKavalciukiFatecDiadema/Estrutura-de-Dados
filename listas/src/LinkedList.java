21943f6b0c21bc541a61fc4deaf98fd62b560ecb
JoaoFaculdade <joao.sanches7@fatec.sp.gov.br>
2022-09-28 06:21:51 -0300

    Lista ligada implementação

---
+++ listas/src/LinkedList.java
@@ -0,0 +1,78 @@
+public class LinkedList {
+    private No inicioLista;
+
+    public LinkedList(){
+        this.inicioLista = null;
+    }
+
+
+    public void adicionaInicio(int elemento){
+        No novoNo = new No(elemento);
+        /*Instancia o novo nó com o novo elemento do início da lista*/
+        novoNo.prox = inicioLista;
+        /* Atribui ao atributo novo da classe nó o endereço do nó*/
+        inicioLista = novoNo;
+        /*Atributo inicio da Lista o novo nó*/
+    }
+
+    public int removeInicio(){
+        int resp = -1;
+        
+        if(this.inicioLista == null) {
+            System.out.println("Lista vazia");
+        } else {
+            resp = inicioLista.elemento;
+            this.inicioLista = inicioLista.prox;
+        }
+        
+        return resp;
+    }
+
+    public boolean buscarElemento(int elemento){
+        No auxiliar = inicioLista;
+        //Enquanto n achar o elemento pedido E for diferente de nulo continua a busca
+        while(auxiliar != null && auxiliar.elemento != elemento){
+            auxiliar = auxiliar.prox;
+        }
+
+        if(auxiliar.elemento == elemento) return true;
+        return false;
+    }
+  
+    
+    public String listaReversa(){
+
+        No aux = inicioLista;
+        String resp = "";
+        //Percorre a lista até não ultrapassar o último nó;
+        while(aux != null){
+            //Recebe o nó atual + espaço em branco + a resposta anterior.
+            resp = aux.elemento + " " + resp;
+    
+            //Atualiza o aux pr próx nó;
+            aux = aux.prox;
+        }
+        
+        return"["  + resp + "]";
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder string = new StringBuilder();
+        
+        No auxiliar = inicioLista;
+
+        string.append("[");
+
+        while(auxiliar.prox != null) {
+            string.append(auxiliar.elemento + ", ");
+            auxiliar = auxiliar.prox;
+        }
+
+        if(auxiliar.prox == null) string.append(auxiliar.elemento + "]");
+
+        
+        return string.toString();
+    }
+}
+
