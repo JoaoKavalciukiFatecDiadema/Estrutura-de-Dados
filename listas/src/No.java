e03d80f36c326d87402e0b9acebec8e4682a205b
JoaoFaculdade <joao.sanches7@fatec.sp.gov.br>
2022-09-28 06:19:57 -0300

    Nó lista ligada

---
+++ listas/src/No.java
@@ -0,0 +1,12 @@
+
+public class No {
+    //nome da variavel dado no slide
+    public int elemento;
+    public No prox;
+    
+    public No(int elemento){
+        this.elemento = elemento;
+        this.prox = null;
+    }
+}    
+
