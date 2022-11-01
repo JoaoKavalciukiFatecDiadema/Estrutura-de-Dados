7e62e3467e34e7931996865e41e0bc75f3fbb97a
JoaoFaculdade <joao.sanches7@fatec.sp.gov.br>
2022-11-01 10:42:20 -0300

    Teste árvore binária

---
+++ arvores/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Arvore arvore = new Arvore();
+
+        arvore.adicionaElemento(6);
+        System.out.println( arvore.getRaiz());
+    }
+}
